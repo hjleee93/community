6a1cfe44183321bfe63c1667a9550d1a7b75ddce
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2022-01-05 10:41:49 +0900

    AddGameInfo 파일 추가

---
+++ src/components/pages/studio/AddGameInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content">
+
+
+    <div>
+      본문 세션1
+    </div>
+
+
+    <div>
+      본문 세션2
+    </div>
+      
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddGameInfo",
+};
+</script>
+
+<style scoped>
+
+</style>
